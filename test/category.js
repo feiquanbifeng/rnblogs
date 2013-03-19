d0e651ac04d30c15e1795606dccac6bf1bac7e60
feiquanbifeng <460429192@qq.com>
2013-03-19 12:16:59 +0800

    新增测试类型

--- test/category.js
+++ test/category.js
@@ -1,7 +1,7 @@
 var models = require('../models')
   , Category = models.Category;
 
-var arr = ['架构设计', 'IT业界', '云计算', '数据库', '编程语言', 'Web前端'];
+var arr = ['架构设计', 'IT业界', '云计算', '数据库', '编程语言', 'Web前端', '其他'];
 for (var i = 0; i < 5; i++) {
     var cat = new Category();
     cat.typename = arr[i];
